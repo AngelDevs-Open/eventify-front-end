00d59b3ed87966cc2e0a7f2495f13abeadb863dc
Fabrizio <u201914181@upc.edu.pe>
2025-05-12 13:15:23 -0500

    feat(main): integrate navigation bar in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavigationBarComponent} from './public/components/navigation-bar/navigation-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
