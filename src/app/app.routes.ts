ddd370822c40ffa3edc0b135d0e79a67735bc6b6
OmBRz <omarbrrcl@gmail.com>
2025-05-16 05:06:20 -0500

    feat(router): add route for calendar view.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './public/pages/home/home.component';
+import {CalendarViewComponent} from './events/pages/calendar-view/calendar-view.component';
 
 const baseTitle= 'Eventify'
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent, data:{title: `${baseTitle} | Home`}},
+  {path: 'calendar', component: CalendarViewComponent, data:{title: `${baseTitle} | Calendar`}},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
