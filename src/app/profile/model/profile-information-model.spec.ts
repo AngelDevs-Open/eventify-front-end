a1f7544987bb629d770bea2435eebe5f5f977b68
Dacc03 <u202120569@upc.edu.pe>
2025-05-18 01:43:25 -0500

    chore(profile): remove unused files.

--- src/app/profile/model/profile-information-model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ProfileInformationModel } from './profile-information-model';
-
-describe('ProfileInformationModel', () => {
-  it('should create an instance', () => {
-    expect(new ProfileInformationModel()).toBeTruthy();
-  });
-});
