fae78c126364ca5f7fbb15727a4acd6d22f8325d
Dacc03 <u202120569@upc.edu.pe>
2025-07-01 01:32:37 -0500

    feat(profile): add routes for profile management component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './public/pages/home/home.component';
+import {ProfileManagementComponent} from './profileManagement/pages/profile-management/profile-management.component';
 
 const baseTitle= 'Eventify'
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent, data:{title: `${baseTitle} | Home`}},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'profile', component: ProfileManagementComponent },
+  { path: 'profile/:id', component: ProfileManagementComponent }
 ];
