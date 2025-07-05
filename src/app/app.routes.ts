3a30b9fb55042bfaf481f709bac95bd4e0992cc9
JulyP <u20211d760@upc.edu.pe>
2025-07-05 16:13:38 -0500

    feat(event-management): add social events route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,14 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './public/pages/home/home.component';
 import {CalendarViewComponent} from './events/pages/calendar-view/calendar-view.component';
+import { EventManagementComponent } from './event-manager/pages/event-management/event-management.component';
+
 
 const baseTitle= 'Eventify'
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent, data:{title: `${baseTitle} | Home`}},
   {path: 'calendar', component: CalendarViewComponent, data:{title: `${baseTitle} | Calendar`}},
+  {path: 'social-events', component: EventManagementComponent, data: {title: `${baseTitle} | SocialEvents`} },
   {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
