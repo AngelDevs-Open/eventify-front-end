32e5e0d51affacb248071b181ede302576520d55
Fabrizio <u201914181@upc.edu.pe>
2025-07-06 16:08:09 -0500

    feat(service-item): update service item entity and service

--- src/app/planning/model/service-item.entity.ts
+++ src/app/planning/model/service-item.entity.ts
@@ -3,13 +3,13 @@ import {v4 as uuidv4} from 'uuid';
 export class ServiceItem {
   id:string;
   description:string;
-  quantity:number | null;
-  unitPrice:number | null;
+  quantity:number;
+  unitPrice:number;
   totalPrice:number;
   quoteOrderId:string;
 
-  constructor({id='',description='', quantity=null,unitPrice=null,quoteOrderId=''}){
-    this.id = id?id:uuidv4();
+  constructor({id='',description='', quantity=0,unitPrice=0,quoteOrderId=''}){
+    this.id = id;
     this.description=description;
     this.quantity=quantity;
     this.unitPrice=unitPrice;
--- src/app/planning/services/service-item.service.ts
+++ src/app/planning/services/service-item.service.ts
@@ -16,7 +16,23 @@ export class ServiceItemService extends BaseService<ServiceItem>{
   }
 
   getByQuoteId(quoteId: string): Observable<Array<ServiceItem>> {
-    return this.http.get<Array<ServiceItem>>(`${this.resourcePath()}?quoteOrderId=${quoteId}`, this.httpOptions)
+    return this.http.get<Array<ServiceItem>>(`${this.serverBaseUrl}/quotes/${quoteId}/service-items`, this.httpOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
+  createServiceItem(quoteId:string, serviceItemResource:any):Observable<ServiceItem> {
+
+    return this.http.post<ServiceItem>(`${this.serverBaseUrl}/quotes/${quoteId}/service-items`,JSON.stringify(serviceItemResource),this.httpOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
+  deleteServiceItemByQuoteId(quoteId:string,serviceItemId:string):Observable<any> {
+    return this.http.delete(`${this.serverBaseUrl}/quotes/${quoteId}/service-items/${serviceItemId}`, this.httpOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
+  updateServiceItem(quoteId:string, resource:any, serviceItemId:string):Observable<ServiceItem> {
+    return this.http.put<ServiceItem>(`${this.serverBaseUrl}/quotes/${quoteId}/service-items/${serviceItemId}`,JSON.stringify(resource), this.httpOptions)
       .pipe(retry(2), catchError(this.handleError));
   }
 }
