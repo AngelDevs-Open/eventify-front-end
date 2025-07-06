df24f28805751ab883d13430f887127cae0b8de0
Fabrizio <u201914181@upc.edu.pe>
2025-07-06 16:07:49 -0500

    feat(service-item): update service item create and edit component

--- src/app/planning/components/service-item-create-and-edit/service-item-create-and-edit.component.ts
+++ src/app/planning/components/service-item-create-and-edit/service-item-create-and-edit.component.ts
@@ -1,10 +1,23 @@
-import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  Inject,
+  Input,
+  OnInit,
+  Optional,
+  Output,
+  ViewChild
+} from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {MatButton} from '@angular/material/button';
 import {ServiceItem} from '../../model/service-item.entity';
 import {FormsModule, NgForm} from '@angular/forms';
 import {BaseFormComponent} from '../../../shared/components/base-form.component';
+import {MatDialogClose, MatDialogContent, MatDialogTitle} from '@angular/material/dialog';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-service-item-create-and-edit',
@@ -12,33 +25,57 @@ import {BaseFormComponent} from '../../../shared/components/base-form.component'
     MatFormFieldModule,
     MatInput,
     MatButton,
-    FormsModule
+    FormsModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogClose
   ],
   templateUrl: './service-item-create-and-edit.component.html',
   styleUrl: './service-item-create-and-edit.component.css'
 })
-export class ServiceItemCreateAndEditComponent extends BaseFormComponent implements OnInit {
-  @Input() serviceItem:ServiceItem= new ServiceItem({});
-  @Input() editMode: boolean = false;
-  @Input() quoteOrderId!: string;
-  @Input() serviceFormDisabled: boolean = true;
+export class ServiceItemCreateAndEditComponent extends BaseFormComponent implements OnInit{
+  serviceItem:ServiceItem;
+  editMode: boolean=false;
+  quoteOrderId!: string;
+  serviceFormDisabled: boolean = true;
 
-  @Output() protected serviceItemAddRequested = new EventEmitter<ServiceItem>();
-  @Output() protected serviceItemUpdateRequested = new EventEmitter<ServiceItem>();
-  @Output() protected cancelRequested = new EventEmitter<void>();
+  description!: string;
+  quantity!: number;
+  unitPrice!: number;
+  totalPrice!: number;
+
+  title!:string;
+
+
+  //@Output() protected serviceItemAddRequested = new EventEmitter<ServiceItem>();
+  //@Output() protected serviceItemUpdateRequested = new EventEmitter<ServiceItem>();
+  //@Output() protected cancelRequested = new EventEmitter<void>();
 
   @ViewChild('serviceItemForm',{static:false}) protected serviceItemForm !: NgForm;
 
-  constructor() {
+  constructor(private cdr: ChangeDetectorRef,@Optional() public dialogRef?: MatDialogRef<ServiceItemCreateAndEditComponent>, @Optional() @Inject(MAT_DIALOG_DATA) public data?:any
+  ) {
     super();
     this.serviceItem= new ServiceItem({});
+
+    if (this.data) {
+      this.description = this.data.description;
+      this.quantity = this.data.quantity;
+      this.unitPrice = this.data.unitPrice;
+      this.totalPrice = this.data.totalPrice;
+      this.editMode = this.data.editMode;
+      this.title = this.data.title;
+    }
+    console.log(this.editMode)
   }
 
   ngOnInit(): void {
+
     if (!this.serviceItem) {
       this.serviceItem = new ServiceItem({});
     }
   }
+
 
   private resetEditState(){
     this.serviceItem = new ServiceItem({});
@@ -48,20 +85,21 @@ export class ServiceItemCreateAndEditComponent extends BaseFormComponent impleme
 
   private isValid = ()=>this.serviceItemForm.valid;
 
-  protected isEditMode = ():boolean =>this.editMode;
 
   protected updateTotalPrice(): void {
-    const { quantity, unitPrice } = this.serviceItem;
-    this.serviceItem.totalPrice = (quantity??0) * (unitPrice??0);
+    const quantity = this.quantity;
+    const unitPrice = this.unitPrice;
+    this.totalPrice = (quantity??0) * (unitPrice??0);
   }
 
   protected onSubmit(){
     if(this.isValid()){
-      this.serviceItem.quoteOrderId = this.quoteOrderId;
+      /*this.serviceItem.quoteOrderId = this.quoteOrderId;
       let emitter= this.isEditMode()?this.serviceItemUpdateRequested:this.serviceItemAddRequested;
       emitter.emit(this.serviceItem);
       console.log(this.serviceItem);
-      this.serviceItem = new ServiceItem({ quoteOrderId: this.quoteOrderId });
+      this.serviceItem = new ServiceItem({ quoteOrderId: this.quoteOrderId });*/
+      this.dialogRef?.close({description: this.description, quantity: this.quantity, unitPrice: this.unitPrice, totalPrice: this.totalPrice});
       this.resetEditState();
     }else{
       console.error('Invalid form data');
@@ -69,7 +107,7 @@ export class ServiceItemCreateAndEditComponent extends BaseFormComponent impleme
   }
 
   protected onCancel(){
-    this.cancelRequested.emit();
+    //this.cancelRequested.emit();
     this.resetEditState();
   }
 
