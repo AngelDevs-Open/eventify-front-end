c35ae74da956d1b204939b0b8376b76a3938a4b9
Fabrizio <u201914181@upc.edu.pe>
2025-07-06 16:08:51 -0500

    feat(routes): add routes for quote create and edit components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,15 @@ import {HomeComponent} from './public/pages/home/home.component';
 import {CalendarViewComponent} from './events/pages/calendar-view/calendar-view.component';
 
 const QuoteManagementComponent = ()=> import('./planning/pages/quote-order-management/quote-order-management.component').then(m => m.QuoteOrderManagementComponent);
+const QuoteOrderCreateAndEditComponent= ()=> import('./planning/components/quote-order-create-and-edit/quote-order-create-and-edit.component').then(m => m.QuoteOrderCreateAndEditComponent);
 
 const baseTitle= 'Eventify'
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent, data:{title: `${baseTitle} | Home`}},
   {path:'quotes', loadComponent: QuoteManagementComponent, data:{title: `${baseTitle} | Quotes`}},
+  {path:'quotes/new',loadComponent:QuoteOrderCreateAndEditComponent, data:{title: `${baseTitle} | Quotes`,editMode:false}},
+  {path:'quotes/:quoteId/edit',loadComponent:QuoteOrderCreateAndEditComponent, data:{title: `${baseTitle} | Quotes`,editMode:true}},
   {path: 'calendar', component: CalendarViewComponent, data:{title: `${baseTitle} | Calendar`}},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
