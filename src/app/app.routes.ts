e23cabdba14e7e1343fb6f74e487cc03299988a1
Fabrizio <u201914181@upc.edu.pe>
2025-07-07 19:23:29 -0500

    feat(iam): add authentication guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './public/pages/home/home.component';
 import {CalendarViewComponent} from './events/pages/calendar-view/calendar-view.component';
+import {authenticationGuard} from './iam/services/authentication.guard';
+import {SignInComponent} from './iam/pages/sign-in/sign-in.component';
+import {SignUpComponent} from './iam/pages/sign-up/sign-up.component';
 
 const QuoteManagementComponent = ()=> import('./planning/pages/quote-order-management/quote-order-management.component').then(m => m.QuoteOrderManagementComponent);
 const QuoteOrderCreateAndEditComponent= ()=> import('./planning/components/quote-order-create-and-edit/quote-order-create-and-edit.component').then(m => m.QuoteOrderCreateAndEditComponent);
@@ -9,9 +12,11 @@ const baseTitle= 'Eventify'
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent, data:{title: `${baseTitle} | Home`}},
-  {path:'quotes', loadComponent: QuoteManagementComponent, data:{title: `${baseTitle} | Quotes`}},
-  {path:'quotes/new',loadComponent:QuoteOrderCreateAndEditComponent, data:{title: `${baseTitle} | Quotes`,editMode:false}},
-  {path:'quotes/:quoteId/edit',loadComponent:QuoteOrderCreateAndEditComponent, data:{title: `${baseTitle} | Quotes`,editMode:true}},
+  {path:'quotes', loadComponent: QuoteManagementComponent, data:{title: `${baseTitle} | Quotes`}, canActivate:[authenticationGuard]},
+  {path:'quotes/new',loadComponent:QuoteOrderCreateAndEditComponent, data:{title: `${baseTitle} | Quotes`,editMode:false}, canActivate:[authenticationGuard]},
+  {path:'quotes/:quoteId/edit',loadComponent:QuoteOrderCreateAndEditComponent, data:{title: `${baseTitle} | Quotes`,editMode:true}, canActivate:[authenticationGuard]},
+  { path: 'sign-in',          component:      SignInComponent,              data: { title: `${baseTitle} | Sign-in`} },
+  { path: 'sign-up',          component:      SignUpComponent,              data: { title: `${baseTitle} | Sign-up`} },
   {path: 'calendar', component: CalendarViewComponent, data:{title: `${baseTitle} | Calendar`}},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
